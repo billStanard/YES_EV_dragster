da0976cb9fbebbf98e38515bc76134989379473b
billStanard <56606563+billStanard@users.noreply.github.com>
2021-02-26 13:01:21 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,25 @@ input.onButtonPressed(Button.A, function () {
         `)
     radio.sendString("go")
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "go") {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            # . . . #
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         # # # . .
